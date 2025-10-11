5ea6d756a2e8241ff619e3e20272496ceda23834
EAJ-APP <ealamillos91@gmail.com>
2025-10-11 23:36:10 +0200

    Crear visualization/__init__.py

---
+++ src/visualization/__init__.py
@@ -0,0 +1,8 @@
+"""
+Módulo de visualización para AccurateMetrics
+Incluye herramientas para graficar resultados de análisis
+"""
+
+from .impact_plots import ImpactVisualizer
+
+__all__ = ['ImpactVisualizer']
