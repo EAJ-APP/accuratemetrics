cdaae3bb03ecbf582c946bbc27b93878e1a83070
EAJ-APP <ealamillos91@gmail.com>
2025-10-11 23:33:55 +0200

    Actualizar __init__.py

--- src/analysis/__init__.py
+++ src/analysis/__init__.py
@@ -1,1 +1,8 @@
-#
+"""
+Módulo de análisis para AccurateMetrics
+Incluye herramientas para análisis de impacto causal
+"""
+
+from .causal_impact import CausalImpactAnalyzer
+
+__all__ = ['CausalImpactAnalyzer']
